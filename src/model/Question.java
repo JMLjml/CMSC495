a22f2f7ef29dfc92e90927a678fc3cd533e1c185
JMLjml <johnlasheski@gmail.com>
2015-09-09 16:58:02 -0400

    added stub methods to Question Class

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,5 +1,34 @@
 package model;
 
-public class Question {
+import java.util.ArrayList;
 
+
+
+public class Question {
+  
+  private enum QuestionTypeEnum {
+    MULTIPLE_CHOICE, TRUE_FALSE
+  }
+  
+  private QuestionTypeEnum questionType;
+  private String questionText;
+  private String answerText;
+  private ArrayList<String> multipleChoiceOptions;
+  
+  public Question(QuestionTypeEnum questionType, String questionText,
+      String answerText, ArrayList<String> multipleChoiceOptions) {
+    
+  }
+  
+  public String getQuestionText() {
+    return this.questionText;
+  }
+  
+  public String getAnswerText() {
+    return this.answerText;
+  }
+  
+  public ArrayList<String> getMultipleChoiceOptions() {
+    return this.multipleChoiceOptions;
+  }
 }
