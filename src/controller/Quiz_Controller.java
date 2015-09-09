ccc6ab7448fa7133ad25958145160dcb050dc940
JMLjml <johnlasheski@gmail.com>
2015-09-09 19:50:43 -0400

    Added stub methods to Quiz_Controller class

--- src/controller/Quiz_Controller.java
+++ src/controller/Quiz_Controller.java
@@ -1,22 +1,40 @@
-/**
- * 
- */
 package controller;
 
-/**
- * @author john
- *
- */
+import java.util.ArrayList;
+
+import model.Quiz;
+
 public class Quiz_Controller {
 
-  /**
-   * 
-   */
+  private ArrayList<Quiz> quizzes;
+  private ArrayList<Integer> scores;
+  private int currQuestion;
+  
   public Quiz_Controller() {
     // TODO Auto-generated constructor stub
   }
-
   
+  public ArrayList<Integer> getScores() {
+    return this.scores;
+  }
+  
+  public boolean writeScores() {
+    return false;
+  }
+
+  public void displayNextQuestion() {};
+  
+  public boolean checkAnswer(String answer) {
+    return false;
+  }
+  
+  public void startQuiz(String name) {};
+
+  private boolean loadQuiz(String name) {
+    return false;
+  }
+  
+  private void updateScores() {};
   //Dummy Method to get unit testing working
   public String toString() {
     return "Quiz Controller";
