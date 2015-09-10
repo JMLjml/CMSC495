1e521536252166f2cf234e3fa2328945c992b9ff
JMLjml <johnlasheski@gmail.com>
2015-09-09 20:57:17 -0400

    Made mainMenu and QC private static

--- src/controller/Quiz_Application.java
+++ src/controller/Quiz_Application.java
@@ -3,15 +3,17 @@ package controller;
 import view.MainMenuView;
 
 public class Quiz_Application {
+  
+  private static MainMenuView mainMenu;
+  private static Quiz_Controller qc;
 
   /**
    * Launch the application.
    */
   public static void main(String[] args) {
     
-    MainMenuView mainMenu = new MainMenuView();
-    
-    Quiz_Controller qc = new Quiz_Controller(mainMenu);
+    mainMenu = new MainMenuView();
+    qc = new Quiz_Controller(mainMenu);
     
     qc.launchQuizGame();
   }
