fd32fc162b95f372a85699c2e4ff3c7751c0a771
JMLjml <johnlasheski@gmail.com>
2015-09-09 21:31:04 -0400

    Playing around with buttons

--- src/view/MainMenuView.java
+++ src/view/MainMenuView.java
@@ -3,11 +3,17 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
 
 public class MainMenuView {
 
   private JFrame frame;
-  
+  private JButton btnQuit, btnStart;
   
   
   /**
@@ -31,5 +37,23 @@ public class MainMenuView {
     frame = new JFrame();
     frame.setBounds(100, 100, 450, 300);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    
+    // Playing around with buttons and action listeners
+    btnQuit = new JButton("Quit");
+    btnQuit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("You pressed the quit button");
+      }
+    });
+    frame.getContentPane().add(btnQuit, BorderLayout.WEST);
+    
+    btnStart = new JButton("Start");
+    btnStart.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("You pressed the start button");
+      }
+    });    
+    frame.getContentPane().add(btnStart, BorderLayout.EAST);
   }
 }
