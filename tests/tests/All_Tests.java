056f9100816dde2a490651e38214ae9e4ab7ddc9
JMLjml <johnlasheski@gmail.com>
2015-09-16 17:55:19 -0400

    Added All_Tests suite for running all JUnit tests together

---
+++ tests/tests/All_Tests.java
@@ -0,0 +1,23 @@
+/** 
+ * All_Tests.java
+ * 
+ * JUnit test suitw for running all JUnit tests on the Dino Quiz application.
+ * 
+ * Author(s):
+ * 
+ * John Lasheski
+ * 
+ */
+
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Quiz_Tests.class, Quiz_Controller_Tests.class,})
+
+public class All_Tests {
+
+}
